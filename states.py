ead8f035aeec5009326b84c83a8ee693865d2329
robalb <alberto.ventafridda@gmail.com>
2019-06-14 00:40:17 +0200

    imported tom's changes

--- states.py
+++ states.py
@@ -12,13 +12,50 @@ class FinalState(State):
 
 class MenuState(State):
 
-    #qua inizializzi tutte le variabili che ti servono
-    h = 0
-    #puoi anche generare metodi personalizzati. è importante però
-    #che il primo parametro sia 'this'
-    def myMethod(this, a, b):
-        return a+b
+    p1=0
+    p2=0
+    x=1
+    y=0
+    down=True
+    right=True
+    
+    def genstr(this,p1,p2,x,y):
+        a=[]
+        b=[' ' for i in range(80)]
+        b.append('\n')
+        st=''
+        for i in range (25):
+            a.append(b[:])
+        for i in range (5):
+            (a[p1+i])[0]='|'
+            (a[p2+i])[79]='|'
+        (a[y])[x]='o'
+        for i in range(25):
+            for k in range(81):
+                st+=(a[i])[k]
+        return(st)
 
+    def logic(this):
+        if this.x==78:
+            this.right=False
+            if this.y>this.p2+4 or this.y<this.p2:
+                return(FinalState)
+        elif this.x==1:
+            this.right=True
+            if this.y>this.p1+4 or this.y<this.p1:
+                return(FinalState)
+        if this.right==True:
+            this.x+=1
+        else:
+            this.x-=1
+        if this.y==24:
+            this.right=False
+        elif this.y==0:
+            this.down=True
+        if this.down==True:
+            this.x+=1
+        else:
+            this.x-=1
     #questa funzione viene chiamata solo una volta, al momento
     #della inizializzazione dello stato
     def setup(this):
@@ -26,17 +63,8 @@ class MenuState(State):
 
     #questa funzione viene chiamata un botto di volte al secondo
     def update(this):
-        #imposta il numero di volte al secondo per cui va chiamata
-        #la funzione update. minimo 1 volta al secondo, massimo 999
-        this.tick = 10
-        if this.h < 20:
-            this.h = this.h + 1
-        else:
-            #return [nome classe] passa ad il prossimo stato
-            return FinalState("ciao")
-        screen = "\n" * this.h
-        screen = screen + "-" * 10
-        #questa funzione è tipo print. stampa quello che gli passi
+        # this.logic()
+        screen=this.genstr(this.p1,this.p2,this.x,this.y)
         this.render(screen)
 
     #questa funzione viene chiamata quando un tasto viene schiacciato
